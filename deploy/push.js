d36539e25692c8aef4566e45e03d312ce144eddd
Rafong <rafong886@gmail.com>
2021-08-08 11:21:06 +0800

    feat_删除部署命令_本人用

--- deploy/push.js
+++ deploy/push.js
@@ -7,7 +7,7 @@ const commit = argv[2];
 async function run(){
     let cp = await shell.exec(`cp -rf ${pwd}/config.template.js ${pwd}/config/index.js`);
     if(cp.stderr.indexOf('No such file') > -1) throw('没有指定文件');
-    await shell.exec('git checkout -b develop')
+    // await shell.exec('git checkout -b develop')
     await shell.exec('git add .')
     await shell.exec(`git commit -m ${commit}`);
     await shell.exec('git push origin develop');
